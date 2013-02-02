153bb0cc6c95f23acce0ab05fd4d5dcaa4221a46
stumac1gr <stumac1gr@gmail.com>
2013-02-02 17:47:19 +0000

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Shutterp::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+  root :to => 'Pages#home' 
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
