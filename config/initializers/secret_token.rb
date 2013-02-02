12027ce05449f766ea2bb5279d6693fcb7600ed1
stumac1gr <stumac1gr@gmail.com>
2013-02-02 12:28:19 +0000

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shutterp::Application.config.secret_token = '17c30494ec1279f899238f73c5bf6842f8f4705a2ca1c0fef12b821965d45103b76d3c5133ef1d25b5e517c5d9728df5d3e1e045d6c8ac2bca516dc80908ab40'
